f5abf129d7d8bf25d4bdcb2ce448a2e2588d65bf
digital27-dev <digital27net@gmail.com>
2026-09-17 21:43:06 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,20 +7,40 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('push', function(e) {
+    const data = e.data ? e.data.json() : {};
+    
+    const title = data.title || "Notifikasi Baru";
+    const options = {
+        body: data.body || "Anda mendapat pesan baru.",
+        icon: '/icon-192x192.png', 
+        badge: '/badge-72x72.png', 
+        vibrate: [200, 100, 200],
+        data: { url: data.url || '/chat.html' }
+    };
+
+    e.waitUntil(
+        self.registration.showNotification(title, options)
+    );
 }); 
 
 self.addEventListener('notificationclick', function(e) { 
     e.notification.close(); 
+    
+    const urlToOpen = e.notification.data.url;
+
     e.waitUntil(
         clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
-            if (clientList.length > 0) {
-                let client = clientList[0];
-                for (let i = 0; i < clientList.length; i++) {
-                    if (clientList[i].focused) client = clientList[i];
+            for (let i = 0; i < clientList.length; i++) {
+                let client = clientList[i];
+
+                if (client.url === urlToOpen && 'focus' in client) {
+                    return client.focus();
                 }
-                return client.focus();
             }
-            return clients.openWindow('/');
+      
+            if (clients.openWindow) {
+                return clients.openWindow(urlToOpen);
+            }
         })
     );
 });
