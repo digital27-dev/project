75aba51642de5ab04789a4b3509e3ee3e1d0feb1
digital27-dev <digital27net@gmail.com>
2026-09-17 19:14:36 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
 self.addEventListener('push', function(e) {
-    // Biarkan kosong jika belum pakai Web Push API dari server
 }); 
 
 self.addEventListener('notificationclick', function(e) { 
     e.notification.close(); 
-    // Baris di bawah ini opsional: berfungsi untuk mengembalikan fokus ke tab chat saat notifikasi diklik
     e.waitUntil(
         clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
             if (clientList.length > 0) {
